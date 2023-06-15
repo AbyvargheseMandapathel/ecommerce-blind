9a5c63aceb4e3ddaf45e95e52b170fa671bfa012
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-06-15 23:24:33 +0530

    login signup done

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,6 +2,11 @@ import pyttsx3
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib import messages
+from django.contrib.messages import get_messages
+
+
 
 
 User = get_user_model()
@@ -17,12 +22,25 @@ def login_view(request):
         if user is not None:
             # User credentials are valid, log in the user
             auth_login(request, user)
+            # Text-to-speech
+            engine = pyttsx3.init()
+            engine.say(f"{username} successfully logged in.")
+            engine.runAndWait()
             return redirect('login_success')
         else:
-            # User credentials are invalid, show an error message or redirect to login page with error
-            return render(request, 'login.html', {'error': 'Invalid credentials'})
+            # Text-to-speech
+            engine = pyttsx3.init()
+            engine.say(f"Invalid Credentials")
+            engine.runAndWait()
+            # User credentials are invalid, show an error message and redirect to login page with error
+            messages.error(request, 'Invalid credentials')
+            # return redirect('login')
     else:
+        # Clear any existing error messages
+        storage = get_messages(request)
+        storage.used = True
         return render(request, 'login.html')
+
     
     
 def signup(request):
@@ -51,3 +69,7 @@ def login_success(request):
     
     # Render the template with the username in the context
     return render(request, 'login_success.html', {'username': username})
+
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
